3d9707f4740cfa6614d9d22ba061bfed026b4bda
Gregory Losco <gtlosco@gmail.com>
2019-03-23 15:17:00 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -4,7 +4,7 @@ require 'pry'
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
  binding.pry
- your_hash = {}
+ your_hash = {"hi" => 3}
   
 end
 
